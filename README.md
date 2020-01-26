9d5689ff7cfcaea9d2bcff20cf1b255d06244bdf
Brian Spit <brian@mythenatic.com>
2020-01-26 22:51:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# LIVEU STATS BOT
+
+Adds a chat command to check the current modems and bitrate on the liveu.
+
+## Build Prerequisities
+
+-   [Node.js](http://nodejs.org/) (with NPM)
+
+## Installation from Source
+
+-   `git clone <repository-url>` or use the `clone or download` button above.
+-   Change into the new directory.
+-   `npm install`
+
+## Config
+
+Edit `config.json` to your own settings.
+
+```JSON
+{
+    "liveu": {
+        "email": "YOUR LIVEU EMAIL",
+        "password": "YOUR LIVEU PASSWORD"
+    },
+    "twitch": {
+        "botUsername": "TWITCH BOT USERNAME",
+        "botOauth": "TWITCH BOT OAUTH",
+        "twitchChannel": "YOUR TWITCH CHANNEL",
+        "commands": ["!lustats", "!liveustats", "!lus"]
+    },
+    "nginx": {
+        "stats": "http://localhost/stat",
+        "application": "publish",
+        "key": "live"
+    }
+}
+```
+
+## How to run from source
+
+Run the node app by running: `npm start`.
---
+++ src/app.js
@@ -0,0 +1,170 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const tmi = require("tmi.js");
+const xml2js = require("xml2js");
+
+const config = require("../config.json");
+const browser = require("./browser.json");
+
+async function getAuth() {
+    const { email, password } = config.liveu;
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    await page.goto("https://solo.liveu.tv/login", { waitUntil: "networkidle2" });
+
+    await page.type("input[id='txtUsername']", email);
+    await page.type("input[id='txtPassword']", password);
+    await page.click("button[type='submit']");
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
+
+    const localStorage = await page.evaluate(() => Object.assign({}, window.localStorage));
+
+    fs.writeFileSync(
+        "./browser.json",
+        JSON.stringify({
+            token: JSON.parse(localStorage["ngStorage-access_token"]),
+            unit: JSON.parse(localStorage["ngStorage-mobileunit"])
+        })
+    );
+
+    await browser.close();
+}
+
+async function getUnits() {
+    let response = await fetch("https://lu-central.liveu.tv/luc/luc-core-web/rest/v0/units/" + browser.unit + "/status/interfaces", {
+        credentials: "include",
+        headers: {
+            accept: "application/json, text/plain, */*",
+            "accept-language": "en-US,en;q=0.9",
+            authorization: "Bearer " + browser.token,
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site"
+        },
+        referrer: "https://solo.liveu.tv/dashboard/units/" + browser.unit,
+        referrerPolicy: "no-referrer-when-downgrade",
+        body: null,
+        method: "GET",
+        mode: "cors"
+    });
+
+    if (response.status == 200) {
+        let data = await response.json();
+        let connected = data.filter(e => {
+            return e.connected;
+        });
+
+        connected.forEach(e => {
+            switch (e.port) {
+                case "eth0":
+                    e.port = "Ethernet";
+                    break;
+                case "wlan0":
+                    e.port = "WiFi";
+                    break;
+                case "2":
+                    e.port = "USB1";
+                    break;
+                case "3":
+                    e.port = "USB2";
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        return connected;
+    } else if (response.status == 204) {
+        return [];
+    } else {
+        console.log("Request failed getting a new auth token");
+        await getAuth();
+        await getUnits();
+    }
+}
+
+async function getBitrate() {
+    const { stats, application, key } = config.rtmp;
+    let bitrate = 0;
+
+    const response = await fetch(stats);
+    const data = await response.text();
+
+    if (response.ok) {
+        try {
+            xml2js.parseString(data, (err, result) => {
+                const publish = result.rtmp.server[0].application.find(stream => {
+                    return stream.name[0] === application;
+                }).live[0].stream;
+
+                if (publish == null) {
+                    return 0;
+                } else {
+                    const stream = publish.find(stream => {
+                        return stream.name[0] === key;
+                    });
+
+                    bitrate = Math.round(stream.bw_video[0] / 1024);
+                }
+            });
+        } catch (error) {
+            // Well that didn't work just return 0
+        }
+    }
+
+    return bitrate;
+}
+
+const { botUsername, botOauth, channel, commands } = config.twitch;
+
+const client = new tmi.Client({
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+    identity: {
+        username: botUsername,
+        password: botOauth
+    },
+    channels: [channel]
+});
+
+client.connect();
+
+client.on("message", async (channel, tags, message, self) => {
+    if (commands.includes(message.toLowerCase())) {
+        // (MODEMS) WiFi: 2453 Kbps, USB1: 2548 Kbps, USB2: 2328 Kbps, Ethernet: 2285 Kbps. (TOTAL BITRATE) LiveU to LRT: 10739 Kbps, LRT to RTMP: 8104 Kbps
+        let units = await getUnits();
+
+        if (units.length == 0) {
+            client.say(channel, "LiveU Offline :(");
+            return;
+        }
+
+        let message = "(MODEMS) ";
+        let total = 0;
+
+        units.forEach((e, i) => {
+            message += `${e.port}: ${e.uplinkKbps} Kbps${i != units.length - 1 ? ", " : ""}`;
+            total += e.uplinkKbps;
+        });
+
+        if (total == 0) {
+            client.say(channel, "LiveU Online and Ready");
+            return;
+        }
+
+        client.say(channel, message);
+
+        if ("nginx" in config) {
+            message = `(TOTAL BITRATE) LiveU to LRT: ${total} Kbps, LRT to RTMP: ${await getBitrate()} Kbps`;
+        } else {
+            message = `(TOTAL BITRATE) LiveU to LRT: ${total} Kbps`;
+        }
+
+        client.say(channel, message);
+    }
+});
+
+console.log("Started liveu stats bot");
